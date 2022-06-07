6f6cc12f9dbed6fa8b7c618c7ca2279f94968e54
raj360 <raymondkalumba360@gmail.com>
2022-06-07 13:40:56 +0300

    fixed algorithm folow

--- VigenereCipher.js
+++ VigenereCipher.js
@@ -1,37 +1,9 @@
-// var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-// var key = 'password';
-
-// function VigenèreCipher(key, abc) {
-
-//   const keyword = 'password'.repeat(4).substring(0, 26);
-
-//   const alphabet = abc;
-
-//   this.getCipher = (row,column)=>{
-//       console.log({row,column});
-//      for (var i = 0; i < alphabet.length; i++){
-//         let r =  alphabet.indexOf(row)
-//          for(var j = 0; j < alphabet.length; j++){
-//            let c = alphabet.indexOf(column);
-
-//            console.log({r,c})
-//          }
-//      }
-//   }
-
-//   this.encode = function (str) {
-//     str.split('').forEach(char => {
-//      let index = abc.indexOf(char);
-//     this.getCipher(char, keyword[index] )
-//     });
-//   };
-//   this.decode = function (str) {
-//     //...
-//   };
-// }
-
 function VigenèreCipher(key, abc) {
-  (this.compute = function (str, dir) {
+  const keyword = 'password'.repeat(4).substring(0, 26);
+
+  const alphabet = abc;
+
+  this.getCipher = (str, dir) => {
     var i = 0;
     var result = '';
     var kl = key.length;
@@ -44,12 +16,13 @@ function VigenèreCipher(key, abc) {
     }
 
     return result;
-  }),
-    (this.encode = function (str) {
-      return this.compute(str, 1);
-    });
+  };
+
+  this.encode = function (str) {
+    return this.getCipher(str, 1);
+  };
   this.decode = function (str) {
-    return this.compute(str, -1);
+    return this.getCipher(str, -1);
   };
 }
 
@@ -57,4 +30,4 @@ var alphabet = 'abcdefghijklmnopqrstuvwxyz';
 var key = 'password';
 
 let c = new VigenèreCipher(key, alphabet);
-c.encode('codewars');
+let output = c.decode('rovwsoiv');
