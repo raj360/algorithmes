78d9c580ccab5ebbe64f693b7d44182dc8856f2e
raj360 <raymondkalumba360@gmail.com>
2022-06-08 21:22:36 +0300

    removed unwanted coments

--- leastIntegalExcluded.js
+++ leastIntegalExcluded.js
@@ -1,7 +1,6 @@
 function solution(A) {
-  //   let sorted = A.sort();
   for (let i = 1; i <= A.length + 1; i++) {
-    if (!sorted.includes(i)) {
+    if (!A.includes(i)) {
       return i;
       break;
     }
@@ -9,4 +8,3 @@ function solution(A) {
 }
 
 let result = solution([1, 3, 6, 4, 1, 2]);
-
