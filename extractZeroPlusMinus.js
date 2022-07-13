3f857f083abe57f199c8b520a69aa5e69b241301
raj360 <raymondkalumba360@gmail.com>
2022-07-13 09:00:31 +0300

    more optimal solutions for previous algos

--- extractZeroPlusMinus.js
+++ extractZeroPlusMinus.js
@@ -30,3 +30,17 @@ function plusMinus(arr) {
 plusMinus(arr1);
 
 
+//Some other optimal solution
+ function plusMinus(arr) {
+  let sample = [0, 0, 0];
+
+  arr.forEach((num) => {
+    num > 0 ? sample[0]++ : num < 0 ? sample[1]++ : sample[2]++;
+  });
+
+  sample.forEach((num) => console.log((num / arr.length).toFixed(6)));
+}
+
+let arr = [-4, 3, -9, 0, 4, 1];
+
+plusMinus(arr);  
