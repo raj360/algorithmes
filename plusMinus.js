ec54c44ea41481d11a3bf9e4d6556895f7440011
raj360 <raymondkalumba360@gmail.com>
2022-07-13 09:33:03 +0300

    diagnoalDifference algo

--- plusMinus.js
+++ plusMinus.js
@@ -5,9 +5,11 @@ function plusMinus(arr) {
     num > 0 ? sample[0]++ : num < 0 ? sample[1]++ : sample[2]++;
   });
 
-  sample.map((num) => num / arr.length).forEach((num) => console.log(num.toFixed(6)));
+  sample.forEach((num) => console.log((num / arr.length).toFixed(6)));
 }
 
 let arr = [-4, 3, -9, 0, 4, 1];
 
-plusMinus(arr);
+console.log(plusMinus(arr));
+
+
