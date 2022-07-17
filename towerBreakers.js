ca9c4d8d5ad929821af7bf6899c370e4787bd885
raj360 <raymondkalumba360@gmail.com>
2022-07-17 14:01:56 +0300

    closest number algo and tower Breakers

---
+++ towerBreakers.js
@@ -0,0 +1,21 @@
+function towerBreakers(n, m) {
+  if (n % 2 == 0 && m !== 1) {
+    return 2;
+  } else if (n % 2 != 0 && m !== 1) {
+    return 1;
+  } else if (m === 1) {
+    return 2;
+  }
+}
+
+
+console.log(6 % 2);
+
+const n = 2,
+  m = 6;
+
+const result = towerBreakers(n, m);
+
+console.log(6 % 2);
+
+result;
