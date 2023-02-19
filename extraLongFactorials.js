51ad44eff6f3388e41d9723b9e03ea453e33dbbe
raj360 <raymondkalumba360@gmail.com>
2023-02-19 22:27:30 +0300

    update: count teams problem

--- extraLongFactorials.js
+++ extraLongFactorials.js
@@ -4,14 +4,3 @@ const extraLongFactorials = (n) =>
 const result = extraLongFactorials(25);
 
 result;
-
-//https://talent.remotebase.com/?referral=fVjnAop3DcsVuXrcWzzyDi
-
-// Introduction for a front end engineer role
-// Path: introduction.js
-const introduction = (name, email, phone, skills) => {
-  const intro = `Hello, my name is ${name}. I can be reached at ${email} or ${phone}. I have experience with ${skills.join(
-    ', '
-  )}.`;
-  return intro;
-};
